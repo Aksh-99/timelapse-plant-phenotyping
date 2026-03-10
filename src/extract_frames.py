00ccda994c8a00a561f5e38aa2a69c9f39a9fd43
Aksh-99 <aksharag@Aksharas-MacBook-Pro.local>
2026-03-10 12:57:40 -0400

    organize the frames

--- src/extract_frames.py
+++ src/extract_frames.py
@@ -1,15 +1,18 @@
 import os
 import cv2
 
-os.makedirs("../data/frames", exist_ok=True)
-video = cv2.VideoCapture('video_sample.mp4')
+
+video_path = "data/raw_videos/day_01.mp4"
+output_folder = "data/frames/day_01"
+os.makedirs(output_folder, exist_ok=True)
+video = cv2.VideoCapture(video_path)
 frame_count = 0
 while True:
     ret, frame = video.read()
     if not ret:
         break
     #print("Reading frame...", frame)
-    filename = f"../data/frames/frame_{frame_count}.jpg"
+    filename = f"{output_folder}/frame_{frame_count}.jpg"
     cv2.imwrite(filename, frame)
     frame_count += 1
     cv2.imshow('video',frame)
